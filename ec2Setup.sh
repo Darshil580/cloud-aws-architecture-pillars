eaa68bfb6d013cba9b976a2f39aad2278dc61c91
Darshil Patel <dpatel0599@gmail.com>
2023-11-26 16:02:24 -0400

    ec2 setup config added

---
+++ ec2Setup.sh
@@ -0,0 +1,7 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm --version
+nvm install 14.17.0
+sudo npm install -g typescript
+sudo npm install -g pm2
+sudo npm install
+pm2 start app.js
