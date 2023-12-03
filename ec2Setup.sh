99c689d327d2f60eb72162fb30a4a765b026afb4
Darshil Patel <dpatel0599@gmail.com>
2023-12-03 09:17:43 -0400

    .env for ec2

--- ec2Setup.sh
+++ ec2Setup.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+
+sudo yum install -y git
 sudo dnf install postgresql15
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
+git clone https://github.com/Darshil580/cloud-aws-architecture-pillars.git
+cd cloud-aws-architecture-pillars
 nvm --version
 nvm install 14.17.0
-sudo npm install -g typescript
-sudo npm install -g pm2
-sudo npm install
+npm install -g typescript
+npm install -g pm2
+npm install
+export NODE_ENV=production
+export SECRET=term-cloud
+npm run build-prod
+node your_app.js
+
 pm2 start app.js
