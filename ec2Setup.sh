15c577af1a8526c6c33505c4e91dd1427202307e
Darshil Patel <dpatel0599@gmail.com>
2023-12-04 03:40:09 -0400

    yml content added

--- ec2Setup.sh
+++ ec2Setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 sudo yum install -y git
-sudo dnf install postgresql15
+sudo dnf install -y postgresql15
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
 git clone https://github.com/Darshil580/cloud-aws-architecture-pillars.git
-cd cloud-aws-architecture-pillars
 nvm --version
 nvm install 14.17.0
 npm install -g typescript
 npm install -g pm2
+cd cloud-aws-architecture-pillars
 npm install
 export NODE_ENV=production
 export SECRET=term-cloud
