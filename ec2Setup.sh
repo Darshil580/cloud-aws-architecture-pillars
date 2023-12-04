43f8b45b6338918fb65c73b91b2aa42e68823271
Darshil Patel <dpatel0599@gmail.com>
2023-12-04 15:23:29 -0400

    ec2 config

--- ec2Setup.sh
+++ ec2Setup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-sudo yum install -y git
-sudo dnf install -y postgresql15
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-source ~/.bashrc
-git clone https://github.com/Darshil580/cloud-aws-architecture-pillars.git
-nvm --version
-nvm install 14.17.0
-npm install -g typescript
-npm install -g pm2
-cd cloud-aws-architecture-pillars
+# sudo yum install -y git
+# sudo dnf install -y postgresql15
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# source ~/.bashrc
+# git clone https://github.com/Darshil580/cloud-aws-architecture-pillars.git
+# nvm --version
+# nvm install 14.17.0
+# npm install -g typescript
+# npm install -g pm2
+# cd cloud-aws-architecture-pillars
 npm install
-export NODE_ENV=production
-export SECRET=term-cloud
 npm run build-prod
 node your_app.js
-
 pm2 start app.js
